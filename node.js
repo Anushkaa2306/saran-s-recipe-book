35147bac6785fd607c213e96b598dcfbb6b111a7
Anushkaa2306 <bdm.lokendra@gmail.com>
2024-10-14 20:15:52 +0530

    first commit

---
+++ node.js
@@ -0,0 +1,15 @@
+const box1 = document.querySelector(".box1");
+
+box1.addEventListener("click", () => {
+    window.location.href =" http://127.0.0.1:5500/cakeweb/caramal.html";
+});
+const box2 = document.querySelector(".box2");
+
+box2.addEventListener("click", () => {
+    window.location.href = "http://127.0.0.1:5500/cakeweb/cookie.html";
+});
+const box3 = document.querySelector(".box3");
+
+box3.addEventListener("click", () => {
+    window.location.href = "http://127.0.0.1:5500/cakeweb/straw.html";
+});
